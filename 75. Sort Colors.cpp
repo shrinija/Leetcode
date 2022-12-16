24cbfb04387d0849d94512a906a6d0b9734c3c51
shrinija <98155154+shrinija@users.noreply.github.com>
2022-12-16 13:01:59 +0530

    Create 75. Sort Colors.cpp

---
+++ 75. Sort Colors.cpp
@@ -0,0 +1,37 @@
+-->count 0,1,2
+  //tc O(n)+O(n)
+//......
+
+// 
+-->Dutch national flag algo
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
+        while(mid<=high)
+        {
+           switch(nums[mid])
+           {
+               case 0:
+               {
+                   swap(nums[mid++],nums[low++]);
+                   break;
+               }
+               case 1:
+               {
+                   mid++;
+                   break;
+               }
+               case 2:
+               {
+                  swap(nums[mid],nums[high--]);
+                  break;
+               }
+           }
+        }
+    }
+};
+//tc O(n)
