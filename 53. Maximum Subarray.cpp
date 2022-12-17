272872a2044296cace552e71aafb62a58b9d955b
shrinija <98155154+shrinija@users.noreply.github.com>
2022-12-17 14:01:05 +0530

    Update 53. Maximum Subarray.cpp

--- 53. Maximum Subarray.cpp
+++ 53. Maximum Subarray.cpp
@@ -1,8 +1,8 @@
 -->[-1]
+-->[-3,-1,-2,]                      //see this
 -->[1,4,-1,-7,5,-1,2]
 -->[1,2,-1,-7,5,-1]
-
-
+-->1,2,3,-2,5
 
 
 
@@ -13,6 +13,9 @@
 
 
   --> dp approach  ??
+  -->divide and conquer  ?
+  -->optimized divide and conqure?
+  -->recursive ?
   
   
 //...............
@@ -40,3 +43,36 @@ public:
         return max;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//..............................................
+
+-->We can start with brute-force by trying out every possible sub-array in nums and choosing the one with maximum sum.
+  
+  
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = size(nums), ans = INT_MIN;
+        for(int i = 0; i < n; i++) 
+            for(int j = i, curSum = 0; j < n ; j++) 
+                curSum += nums[j],
+                ans = max(ans, curSum);        
+        return ans;
+    }
+};
+
+
+//............
