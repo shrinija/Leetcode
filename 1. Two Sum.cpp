e2ea4decca1e31c3c66348c6ad247c62ddab178c
shrinija <98155154+shrinija@users.noreply.github.com>
2022-12-19 13:52:09 +0530

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,3 +1,119 @@
+.....................-->when we have pair inside vector
+
+  -->and if we apply sort function ...
+  -->it get sorted according to the...
+  -->first value in pair...
+  
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+
+   int nums[10] = {1, 4, 2, 6, 7, 4, 22, 21, 12, 4};
+   vector<pair<int, int>> v;
+   for (int i = 0; i < 10; i++)
+      v.push_back({nums[i], i});
+   sort(v.begin(), v.end());
+
+   for (int i = 0; i < v.size(); i++)
+   {
+      cout << " " << v[i].first;
+   }
+   cout << endl;
+   for (int i = 0; i < v.size(); i++)
+   {
+      cout << " " << v[i].second;
+   }
+}
+
+
+
+//.........................................................................................
+
+
+//Ist Approach
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>> v;
+        for(int i=0;i<nums.size();i++)
+            v.push_back({nums[i],i});
+        sort(v.begin(),v.end());
+        int i=0;
+        int j=v.size()-1;
+        int idx1=0;
+        int idx2=0;
+        while(i<j)
+        {
+            if(v[i].first+v[j].first==target)
+            {
+                idx1=v[i].second;
+                idx2=v[j].second;
+                break;
+                
+            }
+            if(v[i].first+v[j].first<target)
+                i++;
+            if(v[i].first+v[j].first>target)
+                j--;
+    }
+        return {idx1,idx2};
+    }
+//Time Complexity:O(nlogn)
+//Time Complexity:O(n)
+
+//Second Approach
+
+   vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> v;
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m.find(target-nums[i])!=m.end())
+            {
+                v.push_back(m[target-nums[i]]);
+                v.push_back(i);
+                return v;
+            }
+            else
+                m[nums[i]]=i;
+        }
+       return v;
+    }
+
+//Time Complexity:O(n)
+//Space Complexity:O(n)
+
+//Third Approach
+vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> index;
+        int size=nums.size();
+        for(int i=0;i<size;i++)
+        {
+            int k=target-nums[i];
+            for(int j=i+1;j<size;j++)
+            {
+                if(nums[j]==k)
+                {
+                    index.push_back(i);
+                    index.push_back(j);
+                    break;
+                }
+            }
+            if(index.size()==2)
+                break;
+        }
+        return index;
+    }
+	//Time Complexity: O(n^2)
+
+
+
+
+//...............................................................
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
