09c264484882a1122760f6e2d64b264d6ee080d7
shrinija <98155154+shrinija@users.noreply.github.com>
2023-01-09 17:47:25 +0530

    Create 20. Valid Parentheses.cpp

---
+++ 20. Valid Parentheses.cpp
@@ -0,0 +1,51 @@
+(){}[]
+({[]})
+[
+[[
+]
+]]
+
+acha question hai ...vo if me ka logic sahi hai.....esko aur ache se samajhake karr sakte
+
+//...................................................................................................................................
+
+
+
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        
+         stack<int>check;
+        for(int i=0;i<s.length();i++)
+        {
+            if(check.empty() || s[i]=='(' || s[i]=='{' || s[i]=='[')
+            {
+               check.push(s[i]);
+            }
+            else
+            {
+                char match=check.top();
+                check.pop();
+                if(s[i]==')' && match!='(')
+                {
+                    return 0;
+                }
+                else if(s[i]=='}' && match!='{')
+                {
+                    return 0;
+                }
+                else if(s[i]==']' && match!='[')
+                {
+                    return 0;
+                }
+            }
+        }
+        if(check.empty()!=1)
+        {
+            return 0;
+        }
+        return 1;
+    }
+};
